57a229cf5694b0c9e311f059fb683c9be88cb161
Manan Sharma <mansharm71@gmail.com>
2025-11-19 22:32:51 -0500

    Added auth with clerk

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,8 +1,25 @@
-import InstructorDashboard from './components/InstructorDashboard'
 import './App.css'
+import InstructorDashboard from './components/InstructorDashboard'
+
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import SignInPage from './components/auth/SignInPage';
 
 function App() {
-  return <InstructorDashboard />
+  return (
+    <div>
+      <SignedIn>
+        <div style={{ display: "flex", justifyContent: "flex-end", padding: 20 }}>
+          <UserButton />
+        </div>
+
+        <InstructorDashboard />
+      </SignedIn>
+
+      <SignedOut>
+        <SignInPage />
+      </SignedOut>
+    </div>
+  );
 }
 
 export default App
--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = "pk_test_Z29yZ2VvdXMtZG9scGhpbi03NS5jbGVyay5hY2NvdW50cy5kZXYk"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>
 )
